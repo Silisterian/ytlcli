3b9bdfcf75266c01d133a7d25865c5e47061bd4d
Silisterian <rayan.boudjema24@gmail.com>
2026-02-04 18:54:10 +0100

    basic player using vlc. can play, pause, skip, stop player. basic playlist system with persistancy using json file. playlists can be added and deleted.

--- ytlcli.py
+++ ytlcli.py
@@ -1,12 +1,103 @@
-from dataclasses import dataclass
-from typing import List, Optional
+from ytmanager import YTManager
 
-@dataclass
-class VideoInfo:
-    title: str
-    url: str
-    duration: Optional[int] = None  # Duration in seconds
-    description: Optional[str] = None
-    tags: Optional[List[str]] = None
+def display_welcome_message():
+    print("Welcome to YTLCLI - Your YouTube Command Line Interface!")
+    print("Type 'help' to see available commands.")
+    print("Type 'exit' to quit the application.")
+    print()
 
 
+
+def main():
+    display_welcome_message()
+    yt_manager = YTManager()
+    state = True
+    while state: 
+        cmd = input("ytlcli> ")
+        if cmd == "exit":
+            state = False
+        elif cmd == "help":
+            print("Available commands:")
+            print("  help - Show this help message")
+            print("  exit - Exit the application")
+            print("  pl | playlist - Add a playlist")
+            print("  show playlist - Show all saved playlists")
+            print("  show queue - Show all videos in the current queue")
+            print("  play - Play the first video in the queue")
+            print("  pause - Pause playback")
+            print("  stop - Stop playback")
+            print("  volume <0-100> - Set volume level")
+        elif cmd == "pl" or cmd == "playlist":
+            playlist_url = input("Enter playlist URL: ")
+            videos = yt_manager.fetch_playlist(playlist_url)
+            yt_manager.add_videos(videos)
+            print(f"Added {len(videos)} videos from the playlist.")
+        elif cmd.startswith("save"):
+            try:
+                arguments = cmd.split()[1:]
+                if len(arguments) != 2:
+                    print("Usage: save <playlist_url> <playlist_name>")
+                    continue
+                playlist_name, playlist_url = arguments
+                if not yt_manager.save_playlist(playlist_url, playlist_name):
+                    print(f"Failed to save playlist '{playlist_name}'.")
+                print(f"Playlist '{playlist_name}' saved successfully.")
+            except IndexError:
+                print("Usage: save <playlist_url> <playlist_name>")
+        elif cmd.startswith("del"):
+            try:
+                playlist_name = cmd.split()[1]
+                yt_manager.delete_playlist(playlist_name)
+            except IndexError:
+                print("Usage: del <playlist_name>")
+        elif cmd.startswith("show"):
+            try:
+                arguments = cmd.split()[1]
+                if arguments == "playlist" or arguments == "pl":
+                    yt_manager.show_playlists()
+                elif arguments == "queue":
+                    videos = yt_manager.list_videos()
+                    if not videos:
+                        print("No videos in the playlist.")
+                    else:
+                        for idx, video in enumerate(videos, start=1):
+                            print(f"{idx}. {video.title} - {video.duration}")
+                else:
+                    print(f"Unknown argument: {arguments}. Use 'show playlist' or 'show queue'.")
+            except IndexError:
+                print("Usage: show <playlist|queue>")
+        #CONTROLS PLAYER
+        elif cmd == "play":
+            yt_manager.play_song()
+        elif cmd.startswith("play"):
+            try:
+                arguments = cmd.split()[1:]
+                playlist_name, shuffle = arguments
+                if shuffle == 'rnd':
+                    shuffle = True 
+                else:
+                    shuffle = False
+                videos = yt_manager.fetch_playlistbyname(playlist_name, shuffle)
+                yt_manager.new_queue(videos)
+                yt_manager.play_song()
+            except IndexError:
+                print("Usage: play <playlist_name>")
+        elif cmd.startswith("volume") or cmd.startswith("vol"):
+            try:
+                volume = int(cmd.split()[1])
+                yt_manager.set_volume(volume)
+                print(f"Volume set to {volume}.")
+            except (IndexError, ValueError):
+                print("Usage: volume <0-100>")
+        elif cmd == "pause":
+            yt_manager.pause()
+        elif cmd == "skip":
+            yt_manager.skip()
+        elif cmd == "stop":
+            yt_manager.stop()
+        else:
+           print(f"Unknown command: {cmd}. Type 'help' for a list of commands.")
+        
+        
+if __name__ == "__main__":
+    main()
--- ytmanager.py
+++ ytmanager.py
@@ -1,15 +1,79 @@
+import time, os
+from random import shuffle
+
+import json
 import yt_dlp
+import vlc 
+
 from dataclasses import dataclass
 from typing import List, Optional
+
+@dataclass
+class VideoInfo:
+    title: str
+    url: str
+    duration: Optional[int] = None  # Duration in seconds
+    description: Optional[str] = None
+    tags: Optional[List[str]] = None
+
+
 
 class YTManager:
     def __init__(self):
         self.opts = {
             'quiet': True,
             'extract_flat': True,
+            'format': 'bestaudio',
             'force_generic_extractor': True,
-        },
+        }
         self.queue = []
+        self.instance = vlc.Instance()
+        self.player = self.instance.media_player_new()
+        if not os.path.exists("playlist.json"):
+            with open("playlist.json", 'w') as f:
+                json.dump({}, f)
+        self.playlists = self.load_playlist()
+    
+    def save_playlist(self, url: str, name: str):
+        try:
+            if name in self.playlists:
+                print(f"Playlist '{name}' already exists. Overwriting.")
+                
+            self.playlists[name] = url
+            print(self.playlists)
+            with open("playlist.json", 'w') as f:
+                json.dump(self.playlists, f)
+            return True
+        except Exception as e:
+            print(f"Error saving playlist: {e}")
+            return False
+
+    def delete_playlist(self, name: str):
+        if name in self.playlists:
+            del self.playlists[name]
+            with open("playlist.json", 'w') as f:
+                json.dump(self.playlists, f)
+            print(f"Playlist '{name}' deleted successfully.")
+        else:
+            print(f"Playlist '{name}' not found.")
+    
+    def load_playlist(self):
+        try:
+            with open("playlist.json", 'r', encoding='utf-8') as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            print("Fichier JSON corrompu, création d'un nouveau")
+        
+        # Si le fichier n'existe pas ou est corrompu
+        return {}
+
+    def show_playlists(self):
+        if not self.playlists:
+            print("No playlists available.")
+            return
+        print("Available playlists:")
+        for name, url in self.playlists.items():
+            print(f"{name}: {url}")
 
     def add_video(self, video: VideoInfo):
         self.queue.append(video)
@@ -17,23 +81,74 @@ class YTManager:
     def add_videos(self, videos: List[VideoInfo]):
         self.queue.extend(videos)
 
+    def new_queue(self, videos: List[VideoInfo]):
+        self.queue = videos
+
+    def fetch_playlistbyname(self, name: str, sh: bool = False) -> List[VideoInfo]:
+        url = self.playlists.get(name)
+        if not url:
+            print(f"Playlist '{name}' not found.")
+            return []
+        vids = self.fetch_playlist(url)
+        if not sh:
+            return vids
+        shuffle(vids)
+        return vids
+
     def fetch_playlist(self, playlist_url: str) -> List[VideoInfo]:
-        # Placeholder for actual playlist fetching logic
-        # In a real implementation, this would interact with YouTube's API or use a library like youtube-dl
-        with yt_dlp.YoutubeDL(self.opts) as ydl:
-            info = ydl.extract_info(playlist_url, download=False)
-            fetched_videos = []
-            for entry in info.get('entries', []):
-                video = VideoInfo(
-                    title=entry.get('title'),
-                    url=entry.get('webpage_url'),
-                    duration=entry.get('duration'),
-                    description=entry.get('description'),
-                    tags=entry.get('tags')
-                )
-                fetched_videos.append(video)
+        try:    
+            with yt_dlp.YoutubeDL(self.opts) as ydl:
+                info = ydl.extract_info(playlist_url, download=False, process=True)
+                fetched_videos = []
+                for entry in info.get('entries', []):
+                    video = VideoInfo(
+                        title=entry.get('title'),
+                        url=entry.get('url'),
+                        duration=entry.get('duration'),
+                        description=entry.get('description'),
+                        tags=entry.get('tags')
+                    )
+                    fetched_videos.append(video)
 
-        return fetched_videos
-
+            return fetched_videos
+        except Exception as e:
+            print(f"Error fetching playlist: {e}")
+            return []   
+    
     def list_videos(self) -> List[VideoInfo]:
         return self.queue
+
+    def get_url_song(self, url) -> str:
+        with yt_dlp.YoutubeDL(self.opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            print(info['url'])
+            return info['url']
+
+    def pause(self):
+        self.player.pause()
+    
+    def stop(self):
+        self.player.stop()
+    
+    def set_volume(self, volume):
+        """Volume entre 0 et 100"""
+        self.player.audio_set_volume(volume)
+
+    def skip(self):
+        self.player.stop()
+        self.play_song()
+
+    def play_song(self):
+        current_song = self.queue.pop(0) if self.queue else None
+        if not current_song:
+            print("No songs in the queue to play.")
+            return None
+        url = self.get_url_song(current_song.url)
+        # print(current_song)
+        media = self.instance.media_new(url)
+        self.player.set_media(media)
+
+        self.player.play()
+        time.sleep(1)  # Wait for the player to start
+        return self.player, self.instance
+        
